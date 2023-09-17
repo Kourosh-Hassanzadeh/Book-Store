64c6ebf02a88ce89ddb8a37619dc66fac569e39a
Kourosh Hassanzadeh <kouroshhsz81@gmail.com>
2023-09-17 15:04:26 +0330

    fix home

--- Bookstore/frontend/src/components/Home/Home.jsx
+++ Bookstore/frontend/src/components/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import "./style.css";
 
 const Home = () => {
   const [books, setBooks] = useState([]);
+  const navigate = useNavigate();
   useEffect(() => {
     axios
       .get("http://127.0.0.1:5000/books")
@@ -15,12 +17,16 @@ const Home = () => {
         console.error("error", err);
       });
   }, []);
+  console.log(books);
+  const NavigateToDelete = (book) => {
+    navigate(`/deleteBook/${book.id}`, book.id);
+  };
   return (
     <section>
       <div className="container-fluid home-main">
-        <div class="row justify-content-center">
+        <div className="row justify-content-center">
           {books.map((book, index) => (
-            <div className="card text-center home-book" key={index}>
+            <div className="btn card text-center home-book" key={index}>
               <div className="card-body">
                 <h5 className="card-title">{book.name}</h5>
                 <h6 className="card-subtitle mb-2 text-muted">
@@ -28,6 +34,17 @@ const Home = () => {
                 </h6>
                 <p className="card-text">{book.description}</p>
                 <pre className="card-text">{book.publish_date}</pre>
+              </div>
+              <div className="row">
+                <button
+                  className="btn btn-danger ml-2 home-btn"
+                  onClick={() => NavigateToDelete(book)}
+                >
+                  Delete
+                </button>
+                <button className="btn btn-warning home-btn ml-auto mr-2">
+                  Update
+                </button>
               </div>
             </div>
           ))}
