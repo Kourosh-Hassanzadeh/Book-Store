86c41152e56212ae6343e56739a11b232c192d04
Kourosh Hassanzadeh <kouroshhsz81@gmail.com>
2023-09-17 14:04:10 +0330

    delete paginating

--- Bookstore/frontend/src/components/Home/Home.jsx
+++ Bookstore/frontend/src/components/Home/Home.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import PaginatedData from "../PaginatedData/PaginatedData";
 import axios from "axios";
 import "./style.css";
 
@@ -20,7 +19,18 @@ const Home = () => {
     <section>
       <div className="container-fluid home-main">
         <div class="row justify-content-center">
-          <PaginatedData data={books} itemsPerPage={3} />
+          {books.map((book, index) => (
+            <div className="card text-center home-book" key={index}>
+              <div className="card-body">
+                <h5 className="card-title">{book.name}</h5>
+                <h6 className="card-subtitle mb-2 text-muted">
+                  Author: {book.author}
+                </h6>
+                <p className="card-text">{book.description}</p>
+                <pre className="card-text">{book.publish_date}</pre>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </section>
