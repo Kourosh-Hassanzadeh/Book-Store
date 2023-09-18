ee1fcbc4549ccd87d7ce4f74874ee1f40ab0d624
Kourosh Hassanzadeh <kouroshhsz81@gmail.com>
2023-09-18 22:24:14 +0330

    delete clg

--- Bookstore/frontend/src/components/DeleteBook/DeleteBook.jsx
+++ Bookstore/frontend/src/components/DeleteBook/DeleteBook.jsx
@@ -6,7 +6,6 @@ import "./style.css";
 const DeleteBook = () => {
   const { id } = useParams();
   const navigate = useNavigate();
-  console.log(id);
   const handleDelete = () => {
     axios
       .delete(`http://127.0.0.1:5000/book/${id}`)
