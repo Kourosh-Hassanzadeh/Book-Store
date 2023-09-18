2eff892cd22e17148d21f6e47473e1aac54f58c3
Kourosh Hassanzadeh <kouroshhsz81@gmail.com>
2023-09-18 14:00:27 +0330

    navbar fixed fo logout

--- Bookstore/frontend/src/components/Navbar/Navbar.jsx
+++ Bookstore/frontend/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
 import { HiOutlineHome } from "react-icons/hi";
 import { FiLogIn } from "react-icons/fi";
+import { CgLogOut } from "react-icons/cg";
 import { MdOutlinePostAdd } from "react-icons/md";
+import { AuthContext } from "../../context/AuthContext";
 import "./style.css";
 
 const Navbar = () => {
+  const { isLoggedIn, logout } = useContext(AuthContext);
+
+  const handleLogout = () => {
+    logout();
+  };
   return (
     <nav className="navbar navbar-expand-lg navbar-light navBg">
       <button
@@ -35,13 +42,25 @@ const Navbar = () => {
           </li>
         </ul>
         <ul className="navbar-nav ml-auto">
-          <FiLogIn className="nav-icons" size={20} />
-
-          <li className="nav-item active">
-            <NavLink className="nav-link" to="/">
-              Login
-            </NavLink>
-          </li>
+          {isLoggedIn ? (
+            <>
+              <CgLogOut className="nav-icons" size={20} />
+              <li className="nav-item active">
+                <NavLink className="nav-link" to="/" onClick={handleLogout}>
+                  Logout
+                </NavLink>
+              </li>
+            </>
+          ) : (
+            <>
+              <FiLogIn className="nav-icons" size={20} />
+              <li className="nav-item active">
+                <NavLink className="nav-link" to="/login">
+                  Login
+                </NavLink>
+              </li>
+            </>
+          )}
         </ul>
       </div>
     </nav>
